4409b8e1eaa8e60533ac28b00a6e1cadcaedba25
vakhtang melikidze <mr.melikidze@gmail.com>
2022-10-05 22:29:22 +0400

    updated "hello today massege"

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
 
-            Console.WriteLine("hello today");
+            Console.WriteLine("Hello today");
         }
     }
 }
